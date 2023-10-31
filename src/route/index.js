9f4d1cf2c99d02330c2cca84b6c6e5195f628f5d
SRHUU <sergiychervinko@gmail.com>
2023-10-31 19:21:00 +0200

    created product crud

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,10 @@ const router = express.Router()
 
 // ================================================================
 
+// _U_S_E_R_
+
+// ================================================================
+
 class User {
   static #list = []
 
@@ -136,6 +140,262 @@ router.post('/user-update', function (req, res) {
   })
 })
 
+// ================================================================
+
+class Product {
+  static #list = []
+
+  constructor(name, price, descritption) {
+    this.id = Math.floor(Math.random() * 90000 + 10000)
+    this.createDate = new Date().toISOString()
+    this.name = name
+    this.price = price
+    this.description = descritption
+  }
+
+  static getList = () => this.#list
+
+  static add = (product) => {
+    this.#list.push(product)
+  }
+
+  static getById = (id) =>
+    this.#list.find((product) => product.id === id)
+
+  static updateById = (id, data) => {
+    const product = this.getById(id)
+
+    if (product) {
+      this.update(product, data)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static update = (
+    product,
+    { price, name, description },
+  ) => {
+    if (price) {
+      product.price = price
+    }
+    if (name) {
+      product.name = name
+    }
+    if (description) {
+      product.description = description
+    }
+  }
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (product) => product.id === id,
+    )
+
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+// ================================================================
+
+// _P_R_O_D_U_C_T_
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/product-create', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // const list = Product.getList()
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('product-create', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'product-create',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.post('/product-create', function (req, res) {
+  const { name, price, description } = req.body
+
+  const product = new Product(name, price, description)
+
+  Product.add(product)
+
+  if (
+    !product.name ||
+    !product.price ||
+    !product.description
+  ) {
+    result = false
+    text = false
+  } else {
+    result = true
+    text = true
+  }
+
+  res.render('alert', {
+    style: 'alert',
+    info: result
+      ? 'Успішне виконання дії'
+      : 'Помилка виконання дії',
+    comment: text
+      ? 'Товар було успішно створено'
+      : 'Товар не було створено',
+  })
+})
+
+// ================================================================
+
+router.get('/product-list', function (req, res) {
+  const products = Product.getList()
+
+  res.render('product-list', {
+    style: 'product-list',
+    data: {
+      products,
+
+      // {
+      //   name: 'Стильна сукня',
+      //   description:
+      //     'Елегантна сукня з натуральної тканини для особливих випадків.',
+      //   price: 1500,
+      // },
+      // {
+      //   name: 'Спортивні кросівки',
+      //   description:
+      //     'Зручні та стильні кросівки для активного способу життя.',
+      //   price: 1200,
+      // },
+      // {
+      //   name: 'Сонячні окуляри',
+      //   description:
+      //     'Модні окуляри з високоякісними лінзами для захисту чей від сонця.',
+      //   id: 2468135790,
+      //   price: 800,
+      // },
+      // {
+      //   name: 'Чоловічий годинник',
+      //   description:
+      //     'Елегантний годинник з мехнічним механізмом та сталевим браслетом.',
+      //   id: 8024679135,
+      //   price: 2500,
+      // },
+      // {
+      //   name: 'Жіночий рюкзак',
+      //   description:
+      //     'Стильний рюкзак з великим відділення та кишенями.',
+      //   price: 900,
+      // },
+      // {
+      //   name: 'Парасолька',
+      //   description:
+      //     'Компактна парасолька з автоматичним механізмом.',
+
+      //   price: 350,
+      // },
+      // {
+      //   name: 'Столові прибори',
+      //   description:
+      //     'Набір столових приборів зі сталі, виготовлені в класичному стилі.',
+      //   price: 600,
+      // },
+      // {
+      //   name: 'Шкіряний гаманець',
+      //   description:
+      //     'Елегантний гаманець з натуральної шкіри з багтьма відділеннями.',
+      //   price: 400,
+      // },
+      // {
+      //   name: 'Фітнес-браслет',
+      //   description:
+      //     'Браслет для вдстеження активності та здоров`я.',
+      //   price: 700,
+      // },
+    },
+  })
+})
+
+// ================================================================
+
+router.get('/product-edit', function (req, res) {
+  const { id } = req.query
+
+  const product = Product.getById(Number(id))
+  console.log(product)
+
+  if (product) {
+    res.render('product-edit', {
+      style: 'product-edit',
+      product,
+    })
+  } else {
+    res.render('alert', {
+      style: 'alert',
+      info: 'Помилка виконання дії',
+      comment: 'Товар з таким ID не знайдено',
+    })
+  }
+})
+// ================================================================
+
+router.post('/product-edit', function (req, res) {
+  const { name, price, description, id } = req.body
+
+  const product = Product.updateById(Number(id), {
+    name,
+    price,
+    description,
+  })
+
+  if (product) {
+    res.render('alert', {
+      style: 'alert',
+      info: 'Оновлення успішне',
+      comment: 'Дані товару були оновлені',
+    })
+  } else {
+    res.render('alert', {
+      style: 'alert',
+      info: 'Помилка виконання дії',
+      comment: 'Товар з таким ID не знайдено',
+    })
+  }
+})
+
+// ================================================================
+
+router.get('/product-delete', function (req, res) {
+  const { id } = req.query
+  console.log(id)
+
+  const product = Product.deleteById(Number(id))
+
+  if (product) {
+    res.render('alert', {
+      style: 'alert',
+      info: 'Видалення успішне',
+      comment: 'Товар був успішно видалений',
+    })
+  } else {
+    res.render('alert', {
+      style: 'alert',
+      info: 'Помилка виконання дії',
+      comment: 'Товар з таким ID не знайдено',
+    })
+  }
+})
 // ================================================================
 
 // Підключаємо роутер до бек-енду
